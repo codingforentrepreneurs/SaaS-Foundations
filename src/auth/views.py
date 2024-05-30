71e750992db4e06ff77bea330d5832dece7feb2b
CodingForEntrepreneurs <hello@teamcfe.com>
2024-05-30 14:12:07 -0600

    5 - Register Users the Hard Way

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
@@ -16,5 +21,17 @@ def login_view(request):
     return render(request, "auth/login.html", {})
 
 
-# def register_view(request):
-#     return render(request, "auth/login.html", {})
+def register_view(request):
+    if request.method == "POST":
+        # print(request.POST)
+        username = request.POST.get("username") or None
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None
+        # Django Forms
+        # username_exists = User.objects.filter(username__iexact=username).exists()
+        # email_exists = User.objects.filter(email__iexact=email).exists()
+        try:
+            User.objects.create_user(username, email=email, password=password)
+        except:
+            pass
+    return render(request, "auth/register.html", {})
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -22,6 +22,7 @@ from .views import home_view, about_view
 urlpatterns = [
     path("", home_view), #index page -> root page
     path("login/", auth_views.login_view),
+    path("register/", auth_views.register_view),
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
