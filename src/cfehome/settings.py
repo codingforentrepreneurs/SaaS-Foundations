b541e23159743438590845a05dac174dd5e25f4c
CodingForEntrepreneurs <hello@teamcfe.com>
2024-05-31 14:32:00 -0600

    8 - Better UI for Django AllAuth

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -71,9 +71,11 @@ INSTALLED_APPS = [
     "commando",
     "visits",
     # third-party-apps
+    "allauth_ui",
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
