aac9f23896f8f61b857b862d5d35fb2cea6c8d2d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-01 14:20:13 -0600

    6 - Django User Permissions in Templates

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:username>/", views.profile_view),
+    path("", views.profile_list_view),
+    path("<str:username>/", views.profile_detail_view),
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,23 +5,22 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+@login_required
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    return render(request, "profiles/list.html", context)
+
 
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
-    print('user.has_perm("visits.view_pagevisit")', user.has_perm("visits.view_pagevisit"))
-
-    # <app_label>.view_<model_name>
-    # <app_label>.add_<model_name>
-    # <app_label>.change_<model_name>
-    # <app_label>.delete_<model_name>
-
-    # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
-    if is_me:
-        if user.has_perm("visits.view_pagevisit"):
-            pass
-            # qs = PageVist.objects.all()
-    return HttpResponse(f"Hello there {username} - {profile_user_obj.id} - {user.id} - {is_me}")
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    return render(request, "profiles/detail.html", context)
