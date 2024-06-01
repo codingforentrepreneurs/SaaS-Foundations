2d4a642f843b73cb6f95b6c93099a5bda3378960
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-01 13:16:59 -0600

    5 - User Permissions and has_perm

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -9,7 +9,19 @@ User = get_user_model()
 @login_required
 def profile_view(request, username=None, *args, **kwargs):
     user = request.user
+    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
+    print('user.has_perm("visits.view_pagevisit")', user.has_perm("visits.view_pagevisit"))
+
+    # <app_label>.view_<model_name>
+    # <app_label>.add_<model_name>
+    # <app_label>.change_<model_name>
+    # <app_label>.delete_<model_name>
+
     # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
+    if is_me:
+        if user.has_perm("visits.view_pagevisit"):
+            pass
+            # qs = PageVist.objects.all()
     return HttpResponse(f"Hello there {username} - {profile_user_obj.id} - {user.id} - {is_me}")
