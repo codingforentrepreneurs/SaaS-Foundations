2d28480d60811201119cb6505f1eb0f55b7d646a
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-01 12:05:54 -0600

    1 - Password Protected Page with Django Sessions

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view
+from .views import home_view, about_view, pw_protected_view
 
 urlpatterns = [
     path("", home_view, name='home'), #index page -> root page
@@ -27,5 +27,6 @@ urlpatterns = [
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
     path('accounts/', include('allauth.urls')),
+    path('protected/', pw_protected_view),
     path("admin/", admin.site.urls),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -48,3 +48,17 @@ def my_old_home_page_view(request, *args, **kwargs):
     # html_file_path = this_dir / "home.html"
     # html_ = html_file_path.read_text()
     return HttpResponse(html_)
+
+VALID_CODE = "abc123"
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    # print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
