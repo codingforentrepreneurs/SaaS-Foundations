3ab57cf9462312804f05f846c905d5546473c03e
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-01 12:14:07 -0600

    2 - User-required Pages via login_required Decorator

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
-from .views import home_view, about_view, pw_protected_view
+from .views import (
+    home_view, 
+    about_view, 
+    pw_protected_view,
+    user_only_view
+)
 
 urlpatterns = [
     path("", home_view, name='home'), #index page -> root page
@@ -27,6 +32,7 @@ urlpatterns = [
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
     path('accounts/', include('allauth.urls')),
+    path('protected/user-only/', user_only_view),
     path('protected/', pw_protected_view),
     path("admin/", admin.site.urls),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,5 +1,6 @@
 import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from visits.models import PageVisit
@@ -62,3 +63,8 @@ def pw_protected_view(request, *args, **kwargs):
     if is_allowed:
         return render(request, "protected/view.html", {})
     return render(request, "protected/entry.html", {})
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
