b0cd01b57a9ec8f2b43da1fc481f1542e7546ae5
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-06 11:23:55 -0600

    23 - Subscription Refresh Utility Function

--- src/subscriptions/utils.py
+++ src/subscriptions/utils.py
@@ -1,8 +1,32 @@
 import helpers.billing
 
+from django.db.models import Q
 from customers.models import Customer
-from subscriptions.models import Subscription, UserSubscription
+from subscriptions.models import Subscription, UserSubscription, SubscriptionStatus
 
+
+def refresh_active_users_subscriptions(user_ids=None):
+    active_qs_lookup = (
+        Q(status = SubscriptionStatus.ACTIVE) |
+        Q(status = SubscriptionStatus.TRIALING)
+    )
+    qs = UserSubscription.objects.filter(active_qs_lookup)
+    if isinstance(user_ids, list):
+        qs = qs.filter(user_id__in=user_ids)
+    elif isinstance(user_ids, int):
+        qs = qs.filter(user_id__in=[user_ids])
+    elif isinstance(user_ids, str):
+        qs = qs.filter(user_id__in=[user_ids])
+    complete_count = 0
+    qs_count = qs.count()
+    for obj in qs:
+        if obj.stripe_id:
+            sub_data = helpers.billing.get_subscription(obj.stripe_id, raw=False)
+            for k,v in sub_data.items():
+                setattr(obj, k, v)
+            obj.save()
+            complete_count += 1
+    return complete_count == qs_count
 
 def clear_dangling_subs():
     qs = Customer.objects.filter(stripe_id__isnull=False)
--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -5,18 +5,18 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from subscriptions.models import SubscriptionPrice, UserSubscription
+from subscriptions import utils as subs_utils
 
 @login_required
 def user_subscription_view(request,):
     user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
     if request.method == "POST":
         print("refresh sub")
-        if user_sub_obj.stripe_id:
-            sub_data = helpers.billing.get_subscription(user_sub_obj.stripe_id, raw=False)
-            for k,v in sub_data.items():
-                setattr(user_sub_obj, k, v)
-            user_sub_obj.save()
-        messages.success(request, "Your plan details have been refreshed.")
+        finished = subs_utils.refresh_active_users_subscriptions(user_ids=[request.user.id])
+        if finished:
+            messages.success(request, "Your plan details have been refreshed.")
+        else:
+            messages.error(request, "Your plan details have not been refreshed, please try again.")
         return redirect(user_sub_obj.get_absolute_url())
     return render(request, 'subscriptions/user_detail_view.html', {"subscription": user_sub_obj})
 
